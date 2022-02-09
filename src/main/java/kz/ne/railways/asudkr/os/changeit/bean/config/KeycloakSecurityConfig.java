ee5cb73f0a6a7d780cd0865e1d402a9c3f2da628
kzhunusov <kaydar91@gmail.com>
2022-02-09 15:51:33 +0600

    refactored security

--- src/main/java/kz/ne/railways/asudkr/os/changeit/bean/config/KeycloakSecurityConfig.java
+++ src/main/java/kz/ne/railways/asudkr/os/changeit/bean/config/KeycloakSecurityConfig.java
@@ -22,7 +22,7 @@ public class KeycloakSecurityConfig extends KeycloakWebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests().antMatchers("/servlet/**").authenticated().anyRequest().permitAll();
         http.csrf().disable();
     }
 

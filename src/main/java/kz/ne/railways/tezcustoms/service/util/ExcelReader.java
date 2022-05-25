6fd70da3f68a91fa81fd1cf1883df9ee0dbb1bc0
Nurzhan_Shyngys <nurzhanshyngys@gmail.com>
2022-05-25 16:27:53 +0600

    TNVED description null check

--- src/main/java/kz/ne/railways/tezcustoms/service/util/ExcelReader.java
+++ src/main/java/kz/ne/railways/tezcustoms/service/util/ExcelReader.java
@@ -102,6 +102,6 @@ public class ExcelReader {
                 break;
             }
         }
-        return result.toString();
+        return result != null ? result.toString() : null;
     }
 }

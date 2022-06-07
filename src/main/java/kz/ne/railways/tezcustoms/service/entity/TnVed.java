3d0b3acc3d965c7db001cd6136d56d78a8954a35
Zhassulan_Baimanov <zh.baimanov@gmail.com>
2022-06-07 17:11:14 +0600

    TEZ-71 creating TnVed entity

---
+++ src/main/java/kz/ne/railways/tezcustoms/service/entity/TnVed.java
@@ -0,0 +1,50 @@
+package kz.ne.railways.tezcustoms.service.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serial;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tn_ved", schema = "TEZ")
+public class TnVed extends AbstractAuditingEntity {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private Long id;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TnVed next;
+
+    @Column
+    private Integer level;
+
+    @Column
+    private String code;
+
+    @Column
+    private String codeEx;
+
+    @Column(columnDefinition = "TEXT")
+    private String text;
+
+    @Column(columnDefinition = "TEXT")
+    private String textEx;
+
+    @Column
+    private String unit;
+
+    @Column
+    private String unitCode;
+
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    private TnVed parent;
+}

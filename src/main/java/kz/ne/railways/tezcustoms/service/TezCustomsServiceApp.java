93c0efe92e9700ea2d4b6a7b64ad1d780081f050
Zhassulan_Baimanov <zh.baimanov@gmail.com>
2022-06-15 16:26:45 +0600

    app name

--- src/main/java/kz/ne/railways/tezcustoms/service/TezCustomsServiceApp.java
+++ src/main/java/kz/ne/railways/tezcustoms/service/TezCustomsServiceApp.java
@@ -3,12 +3,8 @@ package kz.ne.railways.tezcustoms.service;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,10 +12,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableConfigurationProperties
 @EnableScheduling
 @EnableJpaRepositories({"kz.ne.railways.tezcustoms.service"})
-public class SpringBootApp {
+public class TezCustomsServiceApp {
     public static void main(String[] args) {
 
-        SpringApplication springApplication = new SpringApplication(SpringBootApp.class);
+        SpringApplication springApplication = new SpringApplication(TezCustomsServiceApp.class);
         springApplication.setAddCommandLineProperties(false);
 
         springApplication.setBannerMode(Banner.Mode.CONSOLE);

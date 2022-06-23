bd0f6777cd775b0624ae1e92f5cdc70ee2a181a0
Nurzhan_Shyngys <nurzhanshyngys@gmail.com>
2022-06-23 14:46:16 +0600

    TEZ-124 Справочник ТН ВЭД (альта ру)

--- src/main/java/kz/ne/railways/tezcustoms/service/controller/DictionaryController.java
+++ src/main/java/kz/ne/railways/tezcustoms/service/controller/DictionaryController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
